6aa26c7c458085fdb0f3f8828163c772c12a5a35
FaizanJatt <xmuhammadfaizanx@gmail.com>
2023-11-28 16:03:44 +0500

    k

---
+++ src/App.jsx
@@ -0,0 +1,172 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useMemo, useState } from "react";
+import "./App.css";
+import { Amplify } from "aws-amplify";
+import { FaceLivenessDetector } from "@aws-amplify/ui-react-liveness";
+import awsexports from "./aws-exports";
+import axios from "axios";
+import { useRef } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  useSearchParams,
+  useParams,
+} from "react-router-dom";
+
+Amplify.configure(awsexports);
+
+function App() {
+  const [sessionID, setSessionID] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const params = useParams();
+  const [searchParams] = useSearchParams();
+  const token = searchParams.get("id");
+  // console.log(params, searchParams.get("id"));
+  // console.log(searchParams.get("id"));
+
+  const [authToken, setAuthToken] = useState();
+
+  const API = "https://apostrfy.herokuapp.com/api/v1";
+  const login = `${API}/auth/login`;
+
+  async function getAuthId() {
+    try {
+      setIsLoading(true);
+      const response = await axios.post(login, {
+        userCode: "h01io",
+        password: "Test1234!",
+      });
+      console.log(response.data.data.accessToken);
+      setAuthToken(response.data.data.accessToken);
+    } catch (err) {
+      console.log(err);
+      console.log("Something went wrong");
+    }
+  }
+
+  useEffect(() => {
+    const result = searchParams.get("id");
+    setAuthToken(result);
+    console.log("authTokenStored:", result || null);
+  }, []);
+
+  // const injectedObject = window.ReactNativeWebView.injectedObjectJson();
+
+  // const authToken = useMemo(() => {
+  //   if (!window.accessToken) {
+  //     return JSON.parse(injectedObject).token;
+  //   }
+  //   return window.accessToken;
+  // }, [window.accessToken, injectedObject]);
+
+  const handleAnalysis = async () => {
+    // window.ReactNativeWebView.postMessage("verifying");
+    console.log("handle analysis");
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${authToken}`,
+      },
+    };
+    try {
+      const response = await axios.get(
+        "https://apostrfy.herokuapp.com/api/v1/biometrics/get-result",
+        config
+      );
+      const data = response.data;
+      if (typeof data === "string") {
+        console.log(data);
+        // window.ReactNativeWebView.postMessage(`data: ${data}`);
+      } else {
+        console.log(data);
+        // window.ReactNativeWebView.postMessage(JSON.stringify(data));
+      }
+
+      if (data.confidence >= 75) {
+        console.log("success", data.confidence);
+        location.href = "apostrfy://success";
+        // window.ReactNativeWebView.postMessage("Pass");
+      } else {
+        console.log("failure", data.confidence);
+        // window.ReactNativeWebView.postMessage("Fail");
+        // location.href = 'apostrfy://error';
+      }
+    } catch (error) {
+      console.log("failure", error);
+
+      // window.ReactNativeWebView.postMessage("Fail");
+    }
+  };
+
+  const getSessionID = async () => {
+    // console.log(authToken);
+    // console.log(authToken.split(" ")[0]);
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${authToken}`,
+      },
+    };
+    try {
+      const response = await axios.get(
+        "https://apostrfy.herokuapp.com/api/v1/biometrics/get-session",
+        config
+      );
+
+      setSessionID(response.data.sessionId);
+      setIsLoading(false);
+    } catch (err) {
+      if (typeof err === "string") {
+        console.log(err);
+        // window.ReactNativeWebView.postMessage(`ERROR: ${err}`);
+      } else {
+        console.log(err);
+        // window.ReactNativeWebView.postMessage(JSON.stringify(err));
+      }
+    }
+  };
+
+  const handleError = (error) => {
+    if (typeof error === "string") {
+      // window.ReactNativeWebView.postMessage(`ERROR: ${error}`);
+    } else {
+      // window.ReactNativeWebView.postMessage(JSON.stringify(error));
+    }
+  };
+
+  useEffect(() => {
+    getSessionID();
+  }, [authToken]);
+
+  useEffect(() => {});
+
+  return (
+    <>
+      <div className="container">
+        {isLoading ? (
+          <>
+            <h2 style={{ color: "red" }}>Loading...</h2>
+          </>
+        ) : (
+          <div>
+            <FaceLivenessDetector
+              sessionId={sessionID}
+              region="ap-south-1"
+              onAnalysisComplete={handleAnalysis}
+              onError={handleError}
+              disableInstructionScreen={true}
+            />
+            <h2 style={{ color: "red", top: "100px", position: "absolute" }}>
+              Showing...
+            </h2>
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/:id",
+    element: <App />,
+  },
+  {
+    path: "/",
+    element: <div>Please enter a valid authID as a param</div>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+    {/* <App /> */}
+  </React.StrictMode>
+);
