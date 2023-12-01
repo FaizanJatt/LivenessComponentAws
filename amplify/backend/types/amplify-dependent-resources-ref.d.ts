7625e633a5ee6e5b857bb135db39cc0e63964141
FaizanJatt <xmuhammadfaizanx@gmail.com>
2023-12-01 16:25:27 +0500

    minor changes

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,14 +1,14 @@
 export type AmplifyDependentResourcesAttributes = {
-  "auth": {
-    "apostrfyamplifyui961b5930961b5930": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "CreatedSNSRole": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
-    }
-  }
-}
+  auth: {
+    apostrfyamplifyui961b5930961b5930: {
+      AppClientID: "string";
+      AppClientIDWeb: "string";
+      CreatedSNSRole: "string";
+      IdentityPoolId: "string";
+      IdentityPoolName: "string";
+      UserPoolArn: "string";
+      UserPoolId: "string";
+      UserPoolName: "string";
+    };
+  };
+};
