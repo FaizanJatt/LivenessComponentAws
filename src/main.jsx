9d758322af64429985be6ee144e99204b660f137
FaizanJatt <xmuhammadfaizanx@gmail.com>
2023-12-01 16:25:58 +0500

    minor changes

--- src/main.jsx
+++ src/main.jsx
@@ -18,6 +18,5 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-    {/* <App /> */}
   </React.StrictMode>
 );
